02cca07b36d61dff633963f2664e1868b268cc8a
nikitaivanov <iwaa0303@mail.ru>
2024-09-18 21:12:55 +0300

    removed exception handling

--- src/main/kotlin/org/example/notion/minio/service/impl/MinioStorageServiceImpl.kt
+++ src/main/kotlin/org/example/notion/minio/service/impl/MinioStorageServiceImpl.kt
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.InputStream
 
-@Slf4j
+
 @Service
 class MinioStorageServiceImpl(
     private val minioClient: MinioClient,
