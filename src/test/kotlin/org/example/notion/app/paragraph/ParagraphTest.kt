600f6be49f360831aa76e6816aae9bc270d9d448
nikitaivanov <iwaa0303@mail.ru>
2024-10-10 21:16:54 +0300

    changed file position

--- src/test/kotlin/org/example/notion/app/paragraph/ParagraphTest.kt
+++ src/test/kotlin/org/example/notion/app/paragraph/ParagraphTest.kt
@@ -1,4 +1,4 @@
-package org.example.notion.paragraph
+package org.example.notion.app.paragraph
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.example.notion.AbstractIntegrationTest
