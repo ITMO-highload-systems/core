d65fadf287aee4e79999bc40a3fa923a60a9b86d
nikitaivanov <iwaa0303@mail.ru>
2024-10-11 14:35:35 +0300

    removed unnecessary annortation

--- src/main/kotlin/org/example/notion/app/paragraph/dto/ParagraphCreateRequest.kt
+++ src/main/kotlin/org/example/notion/app/paragraph/dto/ParagraphCreateRequest.kt
@@ -1,14 +1,11 @@
 package org.example.notion.app.paragraph.dto
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 import org.example.notion.app.paragraph.entity.ParagraphType
 import org.springframework.web.multipart.MultipartFile
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class ParagraphCreateRequest(
     @Min(1)
     val noteId: Long,
