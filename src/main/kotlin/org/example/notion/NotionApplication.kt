90ff8609d9465bda01d0470f50f1f45cdd649c93
nikitaivanov <iwaa0303@mail.ru>
2024-11-02 17:01:45 +0300

    removed reactive client

--- src/main/kotlin/org/example/notion/NotionApplication.kt
+++ src/main/kotlin/org/example/notion/NotionApplication.kt
@@ -4,13 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import reactivefeign.spring.config.EnableReactiveFeignClients
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableFeignClients
-@EnableReactiveFeignClients
 @EnableHystrix
 class NotionApplication
 
