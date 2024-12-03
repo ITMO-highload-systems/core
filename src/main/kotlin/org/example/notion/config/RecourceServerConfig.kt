7e768fc92ec1dea83905728f39235600c3045873
a.s.lushnikova <a.s.lushnikova@tinkoff.ru>
2024-12-03 16:17:31 +0300

    fix

--- src/main/kotlin/org/example/notion/config/RecourceServerConfig.kt
+++ src/main/kotlin/org/example/notion/config/RecourceServerConfig.kt
@@ -20,7 +20,7 @@ class ResourceServerConfig(private val jwtAuthFilter: JwtAuthFilter) {
                 authorize("/actuator/**", permitAll)
                 authorize("/v3/**", permitAll)
                 authorize("/swagger-ui/**", permitAll)
-                authorize("/api/v1/paragraph/paragraphs", permitAll)
+                authorize("/api/v1/paragraph/paragraphs/**", permitAll)
                 authorize(anyRequest, authenticated)
             }
             addFilterBefore<UsernamePasswordAuthenticationFilter>(jwtAuthFilter)
